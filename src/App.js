7fc6c49abe6cdf021c150a1fe72f9e077bf8c60e
AurobindoGupta <aurogpt10@gmail.com>
2022-04-03 20:38:39 +0530

    setup cartPg, separate card component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import HomePg from "./pageContainer/homePg/HomePg";
 import { Routes, Route } from "react-router-dom";
 import ProductPg from "./pageContainer/productPg/ProductPg";
+import CartPg from "./pageContainer/cartPg/CartPg";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePg />} />
         <Route path="/productPg" element={<ProductPg />} />
+        <Route path="/cartPg" element={<CartPg />} />
       </Routes>
     </div>
   );
